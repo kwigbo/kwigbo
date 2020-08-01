153a2efaa863b5b283fd2fcbe1eb88c165bf3c35
Jedidiah Laudenslayer <jlaudenslayer@themeetgroup.com>
2020-08-01 15:06:14 -0400

    Added stickers store link
    Change-Id: I5510cd6b49d70eeddbb9d182b1d7953196cf4cd9

--- src/View/Nav.js
+++ src/View/Nav.js
@@ -7,16 +7,7 @@ export class Nav extends Component {
 	render() {
 		return(
 			<div id="Nav">
-      {/*
-        <ul>
-          <li><a href="">Item 1</a></li>
-          <li><a href="">Item 2</a></li>
-          <li><a href="">Item 3</a></li>
-          <li><a href="">Item 4</a></li>
-          <li><a href="">Item 5</a></li>
-          <li><a href="">Item 6</a></li>
-        </ul>
-      */}
+        <a href="http://shop.kwigbo.com">Stickers!!</a>
       </div>
     )
 	}
