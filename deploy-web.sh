da7418f78bfa67afa852daf83c72f44017bc2427
Jedidiah Laudenslayer <jlaudenslayer@themeetgroup.com>
2021-02-18 12:13:11 -0500

    Remove .htaccess file
    Change-Id: Iacb4fb71044b877a2fb14a0f051554ae63a18116

--- deploy-web.sh
+++ deploy-web.sh
@@ -9,7 +9,6 @@ mkdir ${KWIGBO_ROOT}
 # Copy Phase
 cp index.html ${KWIGBO_ROOT}
 cp manifest.json ${KWIGBO_ROOT}
-cp .htaccess ${KWIGBO_ROOT}
 cp -r js ${KWIGBO_ROOT}
 cp -r favicon ${KWIGBO_ROOT}
 cp -r images ${KWIGBO_ROOT}
