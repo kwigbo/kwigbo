dd31aad6bc2a4c45530d2ce29d215d7eea1ca8b3
Jedidiah Laudenslayer <jlaudenslayer@themeetgroup.com>
2021-02-17 21:36:15 -0500

    Reorganize files
    Change-Id: I7fb27606f8e083bc65c93d309c0c64e1f3d41a22

--- package.sh
+++ package.sh
@@ -6,15 +6,11 @@ mkdir build
 
 # Copy Phase
 cp index.html build
-cp style.css build
-cp manifest.json build
-cp FFFFORWA.TTF build
 cp -r js build
 cp -r favicon build
 cp -r icons build
+cp -r meta build
 cp -r js build
-mkdir build/js-util/
-cp js-util/util.js build/js-util/util.js
 cd build
 
 # Run
