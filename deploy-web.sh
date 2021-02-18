ce0992df727854e0f266b92a24e3424daedd05f2
kwigbo <jed@kwigbo.com>
2021-02-18 17:02:46 +0000

    Modify deploy script for web server.

--- deploy-web.sh
+++ deploy-web.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+KWIGBO_ROOT=/var/www/html/kwigbo
+
 # Clean Phase
-rm -rf build
-mkdir build
+rm -rf ${KWIGBO_ROOT}
+mkdir ${KWIGBO_ROOT}
 
 # Copy Phase
-cp index.html build
-cp manifest.json build
-cp -r js build
-cp -r favicon build
-cp -r images build
-cp -r styles build
-cp -r js build
-cd build
+cp index.html ${KWIGBO_ROOT}
+cp manifest.json ${KWIGBO_ROOT}
+cp -r js ${KWIGBO_ROOT}
+cp -r favicon ${KWIGBO_ROOT}
+cp -r images ${KWIGBO_ROOT}
+cp -r styles ${KWIGBO_ROOT}
+cp -r js ${KWIGBO_ROOT}
